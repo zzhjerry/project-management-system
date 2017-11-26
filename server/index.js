a1bdb8c7d484a3ac5f0a73e10f2e792b6e771a8d
zzh699 <zzh699@gmail.com>
2017-11-26 14:19:37 +0800

    ADD: frontend with create-react-app and setup express to serve it

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 var app = express()
 app.use(require('cookie-parser')())
@@ -10,9 +11,17 @@ const passport = require('passport')
 app.use(passport.initialize())
 app.use(passport.session())
 
+// Serve static assets
+app.use(express.static(path.resolve(__dirname, '../app', 'build')))
+
 app.use('/api/auth', require('./auth.js'))
 app.use('/api/users', require('./users.js'))
 app.use('/api/projects', require('./projects.js'))
+
+// Always return the main index.html, so react-router render the route in the client
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../app', 'build', 'index.html'))
+})
 
 /**
  * Catch-all error handler middleware
