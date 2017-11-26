08117f76f2c11c32a19e3fa5340567da2c40c6d4
zzh699 <zzh699@gmail.com>
2017-11-26 17:08:02 +0800

    ADD: login form

--- app/src/Login.js
+++ app/src/Login.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div style={styles.container}>
+      <h3>Welcome to Lynk Project Admin Panel</h3>
+      <Form style={styles.form}>
+        <FormGroup>
+          <Label for="email">Email</Label>
+          <Input type="email" name="email" id="email" placeholder="Please input your emails address" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="password">Password</Label>
+          <Input minlength="8" type="password" name="password" id="password" placeholder="Please input your password" />
+        </FormGroup>
+        <Button color="primary" outline block size="sm">Login</Button>
+      </Form>
+    </div>
   )
 }
 
+const styles = {
+  container: {
+    marginTop: '50px',
+    textAlign: 'center'
+  },
+
+  form: {
+    textAlign: 'left',
+    width: '400px',
+    margin: '50px auto'
+  }
+}
 export default Login
