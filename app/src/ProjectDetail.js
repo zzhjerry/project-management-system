2dd67522281f8d3757b938d957b9fc75f376868d
zzh699 <zzh699@gmail.com>
2017-11-27 21:55:24 +0800

    ADD: markdown diaplay view

--- app/src/ProjectDetail.js
+++ app/src/ProjectDetail.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { getProjectAsync } from './actions'
+import md from 'markdown-it'
 
 /* components */
 import { Form, Button, Input, Badge } from 'reactstrap'
@@ -11,12 +12,12 @@ class ProjectDetail extends React.Component {
       return <h1>Loading</h1>
     }
 
-    const { title, status } = this.props.project
+    const { title, status, createdAt, description } = this.props.project
     return (
       <div className="w-75 m-auto p-5">
         <ProjectHeader title={title}></ProjectHeader>
-        <ProjectStatus status={status}></ProjectStatus>
-        <Description></Description>
+        <ProjectStatus status={status} createdAt={createdAt}></ProjectStatus>
+        <Description description={description}></Description>
         <ExpertList></ExpertList>
       </div>
     )
@@ -72,21 +73,80 @@ const ProjectStatus = (props) => {
     }
   }
   return (
-    <div className="mb-2">
+    <div className="mb-2 d-flex align-items-center">
       <Badge disabled color={color(props.status)}>
         {props.status}
       </Badge>
+      <small className="ml-2">Created at: {props.createdAt}</small>
     </div>
   )
 }
 
-const Description = ({ editable }) => (
-  <p>description</p>
-)
+class Description extends React.Component {
+  render() {
+    const { editable, description, authenticated } = this.props
+    if (authenticated && editable) {
+      return (
+        <MarkdownEditAndPreview
+          text={description}
+          onSaveButtonClick={this.handleSave}
+          onCancelButtonClick={this.handleCancel} >
+        </MarkdownEditAndPreview>
+      )
+    }
+    return (
+      <MarkdownDisplay
+        text={description}
+        onEditButtonClick={this.toggleEditable}>
+      </MarkdownDisplay>
+    )
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      editable: false
+    }
+    this.toggleEditable = this.toggleEditable.bind(this)
+    this.handleSave = this.handleSave.bind(this)
+    this.handleCancel = this.handleCancel.bind(this)
+  }
+
+  toggleEditable() {
+    this.setState({ editable: !this.state.editable })
+  }
+
+  handleSave() {}
+  handleCancel() {}
+}
 
 const ExpertList = () => (
   <p>experts</p>
 )
+
+const MarkdownEditAndPreview = (props) => {
+  return (
+    <p>Preview</p>
+  )
+}
+
+const MarkdownDisplay = (props) => {
+  const { text = '' } = props
+  const html = md().render(text)
+  const { onEditButtonClick } = props
+  return (
+    <div className="my-4 p-3 border border-left-0 border-right-0 w-75">
+      <div dangerouslySetInnerHTML={{__html:html}} />
+      <div className="d-flex justify-content-between" style={{ fontSize: '12px' }}>
+        <span className="font-italic">Markdown is supported</span>
+        <a onClick={onEditButtonClick}>
+          <i className="fa fa-pencil mr-1"></i>
+          Edit
+        </a>
+      </div>
+    </div>
+  )
+}
 
 const mapStateToProps = state => {
   return {
